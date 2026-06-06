e4946ef1c409c48419c7d0f42176a69448291014
kayke <kaykecristofersc@gmail.com>
2026-06-06 16:51:23 -0300

    feat: adiciona o endpoint para consultar as metas do usuário closes #26

--- backend/src/routes/goals.routes.ts
+++ backend/src/routes/goals.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/auth.middleware.js";
-import { saveGoals } from "../controllers/goals.controller.js";
+import { saveGoals, getGoals } from "../controllers/goals.controller.js";
  
 const router = Router();
  
 router.use(authenticate);
  
 router.post("/", saveGoals);
+
+router.get("/", getGoals);
  
 export default router;
