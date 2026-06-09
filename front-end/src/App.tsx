11e1df57e02723325538ee73cb8902d7aed37a18
vilarinhojoaope <vilarinhojoaope@gmail.com>
2026-06-09 09:13:08 -0300

    :sparkles:feat: configura Stack e Tab Navigator com react navigation e criar tela de seleção de objetivos com componente GoalCard reutilizável

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import {View,Text, TouchableOpacity,StyleSheet,Pressable} from 'react-native'
+import {SafeAreaView} from 'react-native-safe-area-context'
+import { AppRoutes } from './Routes';
+
+export default function App() {
+
+  return(
+    <AppRoutes/>
+  );
+
+}
+
+
---
+++ front-end/src/Routes.tsx
@@ -0,0 +1,30 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { HomePage } from "./screens/Home";
+import { DetailPage } from "./screens/Detail";
+import { GoalsScreen } from "./screens/Goals";
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+// Tab Navigator separado
+const MainTabs = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="home" component={HomePage} />
+            <Tab.Screen name="detail" component={DetailPage} />
+        </Tab.Navigator>
+    );
+};
+
+export const AppRoutes = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Goals" component={GoalsScreen} />
+                <Stack.Screen name="Main" component={MainTabs} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
